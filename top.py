7144135655bb33edafc571746396bb43e7e97160
aviiciii <laavesh1@gmail.com>
2023-06-27 21:15:38 +0530

    update the generator

--- top.py
+++ top.py
@@ -8,9 +8,27 @@ def main():
     # Number of rows
     no_of_rows = (100, 250, 500, 1000, 5000, 10000)
 
-    # Call function
-    for n in no_of_rows:
-        top(n, version)
+    # # Call function to create top csv files
+    # for n in no_of_rows:
+    #     top(n, version)
+
+    # Call function to create summary md file (only do if directory exists)
+    summary(version)
+
+def summary(version):
+    summary = {}
+
+    summary['total_count'] = count_words_above_frequency(version, 0)
+    summary['freq5'] = count_words_above_frequency(version, 5)
+    summary['freq100'] = count_words_above_frequency(version, 100)
+    summary['freq1000'] = count_words_above_frequency(version, 1000)
+
+    # Create a md file
+    with open(f'output/v{version}/summary.md', 'w') as file:
+        summary_text = f'# Summary\n\nTotal count of words: {summary["total_count"]}\n\nFrequency > 5: {summary["freq5"]}\n\nFrequency > 100: {summary["freq100"]}\n\nFrequency > 1000: {summary["freq1000"]}'
+        file.write(summary_text)
+
+    
 
 def top(no_of_rows, version):
 
@@ -45,6 +63,26 @@ def read_n_rows(file_path, n):
                 break
             rows.append(row)
     return rows
+
+
+def count_words_above_frequency(version, n):
+
+    # paths
+    csv_path = f'data/output/filtered/{version}.csv'
+
+    count = 0
+
+    with open(csv_path, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)  # Skip the header row
+
+        for row in reader:
+            frequency = int(row[1])
+
+            if frequency > n:
+                count += 1
+
+    return count
 
 if __name__ == '__main__':
     main()
