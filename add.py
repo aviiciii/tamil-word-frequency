3fc34e4cf91575abb4c3cab6bdb253de3ab4679f
aviiciii <laavesh1@gmail.com>
2023-06-27 16:56:57 +0530

    using dict to reduce memory

--- add.py
+++ add.py
@@ -1,62 +1,40 @@
 import csv
+from collections import defaultdict
 
-# 1, 2, 3 done
+# Paths
+input1 = 'data/input/v2/4.csv'  # smaller file
+input2 = 'data/input/v2/5.csv'  # larger file
+output_path = 'data/output/2_b.csv'  # output file
 
-# paths
-input1 = 'data/input/4.csv' # smaller file
-input2 = 'data/input/5.csv' # larger file
+word_freq = defaultdict(int)  # Dictionary to store word frequencies
 
-output_path = 'data/output/2_a.csv' # output file
-
-words = []
-frequencies = []
-words_read = 0
-
-# read output file
+# Read input1
+print('Reading input1... ', end='')
 with open(input1, 'r') as csv_file:
-    print('Reading input1... ', end='')
-
-    reader = csv.reader(csv_file)
-
+    reader = csv.DictReader(csv_file)
     for row in reader:
-        words.append(row[0].strip())
-        frequencies.append(row[1].strip())
-        words_read += 1
+        print(row)
+        word = row['word'].strip()
+        freq = int(row['frequency'])
+        word_freq[word] += freq
+print('Completed.')
 
-        if words_read % 10000 == 0:
-            print("#", end='')
-
-print(f'\nCompleted. Words read: {words_read}')
-words_read = 0
-
+# Read input2
+print('Reading input2... ', end='')
 with open(input2, 'r') as csv_file:
-    print('Reading input2... ', end='')
-    reader = csv.reader(csv_file)
-
+    reader = csv.DictReader(csv_file)
     for row in reader:
-        words_read += 1
-        # check if word is in list
-        if row[0] in words:
-            # get index of word
-            index = words.index(row[0])
-            # add frequency
-            frequencies[index] = int(frequencies[index]) + int(row[1])
-        else:
-            words.append(row[0])
-            frequencies.append(row[1])
-        if words_read % 10000 == 0:
-            print("#", end='')
+        word = row['word'].strip()
+        freq = int(row['frequency'])
+        word_freq[word] += freq
+print('Completed.')
 
-print(f'\nCompleted. Words read: {words_read}')
-
-    
+# Write to file
 print('Writing to file... ', end='')
-# write to file
-with open(output_path, 'w') as csv_file:
-    writer = csv.writer(csv_file)
-
-    writer.writerow(['word', 'frequency'])
-    for i in range(len(words)):
-        writer.writerow([words[i], frequencies[i]])
-    
+with open(output_path, 'w', newline='') as csv_file:
+    fieldnames = ['word', 'frequency']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    for word, freq in word_freq.items():
+        writer.writerow({'word': word, 'frequency': freq})
 print('Done!')
